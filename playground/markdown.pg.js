ffa39e546f737d7c304d868c3775dd0d12fcdcdf
OwaisMajid <owais.majid@yahoo.com>
2018-05-23 01:40:50 +0300

    fix logs

--- playground/markdown.pg.js
+++ playground/markdown.pg.js
@@ -2,15 +2,14 @@ const http = require('http'),
 fs = require('fs'),
 path = require('path')
 
-const md = require('markdown-it')();
+var md = require('markdown-it')();
 
 http.createServer((req,res)=>{
     res.writeHead(200, {'Content-Type': 'text/html'});
     fs.readFile(path.resolve(__dirname, "test.md"), "utf8", function(err, data){
         if(err){
-            console.log(err);
+            throw err
         }else{
-            console.log(data)
             res.write(md.render(data));
             res.end();
         }
